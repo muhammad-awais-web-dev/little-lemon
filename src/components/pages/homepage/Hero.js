d35d6b81a6cacee7cd55f120d7399351ca22c774
Muhammad Awais <awaisrafique4929@gmail.com>
2025-08-26 12:12:14 +0500

    Home/Hero Done

--- src/components/pages/homepage/Hero.js
+++ src/components/pages/homepage/Hero.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import './Hero.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Hero.css"; // Import styles
 
 function Hero() {
   return (
-    <section className="Hero-Home">
-        <div style={{backgroundColor:'blue'}}>
-            
+    <section className="home-hero">
+      <div className="home-hero-container">
+        <div className="home-hero-content">
+          <h1 className="home-hero-title">
+            Little Lemon <br />
+            <span>Chicago</span>
+          </h1>
+          <p className="home-hero-text">
+            We are a family-owned Mediterranean restaurant, rooted in tradition
+            and inspired by innovation. Our dishes celebrate classic recipes
+            passed down through generations, while adding a fresh, modern twist
+            to create a dining experience that feels both authentic and new.
+          </p>
+          <Link className="home-hero-button button" to="/reservation">
+            Book A Table
+          </Link>
         </div>
-        <div style={{backgroundColor:'green'}}>
 
+        <div className="home-hero-image">
+          <img src="/images/restaurant.webp" alt="Restaurant"/>
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
