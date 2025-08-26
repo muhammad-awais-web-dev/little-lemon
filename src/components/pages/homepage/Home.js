19808590de6ae57045a1ab2eea801e6e0da635ee
Muhammad Awais <awaisrafique4929@gmail.com>
2025-08-26 13:37:53 +0500

    Home/Specials

--- src/components/pages/homepage/Home.js
+++ src/components/pages/homepage/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero'
+import HomeSpecials from './HomeSpecials'
 
 function Home() {
   return (
     <>
       <Hero />
+      <HomeSpecials />
     </>
   )
 }
