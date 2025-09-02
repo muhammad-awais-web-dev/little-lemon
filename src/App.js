a946e4eb88d65bc4898ebb45028a2e6a47f284eb
Muhammad Awais <awaisrafique4929@gmail.com>
2025-09-02 14:20:41 +0500

    testing2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <Router basename='/project/little-lemon'>
+    <Router basename='/little-lemon'>
       <ScrollToTop />
       <Navbar />
       <Routes>
